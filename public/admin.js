5898289ecb0421dd43c5dcb9b2096c2bfd4ceca3
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-03-28 09:33:42 -0400

    completed

--- public/admin.js
+++ public/admin.js
@@ -4,17 +4,21 @@ async function main() {
     let response = await fetch ('http://localhost:3001/listBooks')
     let books = await response.json()
 
-    books.forEach(createBook)
+    books.forEach(newBook)
 }
 
 function newBook(book) {
     let root = document.getElementById("root");
+
     let list = document.createElement("li")
     list.innerHTML = book.title;
+
     let quantityInput = document.createElement("input");
     quantityInput.value = book.quantity;
+
     let save = document.createElement("button");
     save.innerHTML = "Save";
+
     save.addEventListener("click", () => {
         fetch("http://localhost:3001/updateBook", {
             method: "PATCH",
@@ -27,8 +31,11 @@ function newBook(book) {
             }),
           });
         });
+
         list.append(quantityInput, save);
         root.append(list);
+        
       }
+      
       
       main();
